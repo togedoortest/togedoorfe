1702b2041529af0af51f5415f61dcd6a84cdb15d
anas abomoch <anas3506606@gmail.com>
2021-01-23 18:01:17 +0200

    first commit

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -24,11 +24,18 @@ const HomeScreen = (props) => {
       try {
         
         setIsLoading(true);
-        const response = await axios.get("http://togedoorserver-env.eba-666nat8c.us-west-2.elasticbeanstalk.com/services");
+        let myInit = { // OPTIONAL
+          headers: {}, // OPTIONAL
+          response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
+          queryStringParameters: {  // OPTIONAL
+              name: 'param'
+          }}
+
+        const response = await axios.get(myInit,"http://togedoorserver-env.eba-666nat8c.us-west-2.elasticbeanstalk.com/services");
         setServices(response.data);
         setIsLoading(false);
       } catch (error) {
-        console.log(error);
+        console.log(error.response);
       }
     }
     fetchData();
